e57027f0b480c105b00ed16cfd0d1097ba526834
braya <braya@DESKTOP-E5568VR>
2020-11-02 19:15:57 -0500

    Avance del proyecto en el paquete modelo

--- src/ec/edu/ups/modelo/Telefono.java
+++ src/ec/edu/ups/modelo/Telefono.java
@@ -12,5 +12,57 @@ package ec.edu.ups.modelo;
 public class Telefono {
     private int codigo;
     private String num_telf;
+
+    public Telefono(int codigo, String num_telf) {
+        this.codigo = codigo;
+        this.num_telf = num_telf;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNum_telf() {
+        return num_telf;
+    }
+
+    public void setNum_telf(String num_telf) {
+        this.num_telf = num_telf;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Telefono other = (Telefono) obj;
+        if (this.codigo != other.codigo) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Telefono{" + "codigo=" + codigo + ", num_telf=" + num_telf + '}';
+    }
+    
+    
     
 }
